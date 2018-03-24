414b690e5e97d0990c80ee645ad01974a478d27e
rhysr <rhys@mega-city-one.co.uk>
2018-03-24 12:55:59 +0000

    Add static home page

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+$body = 'HOME';
+$title = 'Home';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title><?php echo $title ?>$</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+    <div class="content">
+        <main class="container">
+            <?php echo $body ?>
+        </main>
+    </div>
+</body>
+</html>
