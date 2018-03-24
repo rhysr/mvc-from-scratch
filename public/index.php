c6348af72b09a76d1f1bfbd83b9e6560e83c7bd4
rhysr <rhys@mega-city-one.co.uk>
2018-03-24 14:43:40 +0000

    Refactor view params into "controllers"

--- public/index.php
+++ public/index.php
@@ -8,21 +8,44 @@ $urlPath = $parsedUrl['path'];
 $body = 'UNKNOWN';
 $title = 'Unknown';
 
+$homeController = function() {
+    return [
+        'body' => 'HOME',
+        'title' => 'home',
+    ];
+};
+
+$productController = function() {
+    return [
+        'body' => 'PRODUCT 123456',
+        'title' =>  'Product 123456'
+    ];
+};
+
+$noMatchController = function () {
+    return [
+        'body' => 'UNKNOWN',
+        'title' =>  'Unknown'
+    ];
+
+};
+
+$controller = $noMatchController;
+
 $homeRoute = new \Router\StaticRoute('/');
 $productRoute = new \Router\RegexRoute('#/product/\d+$#');
 
 $homeMatch = $homeRoute->match($urlPath);
 if ($homeMatch->isMatch()) {
-    $body = 'HOME';
-    $title = 'Home';
+    $controller = $homeController;
 } else {
     $productMatch = $productRoute->match($urlPath);
     if ($productMatch->isMatch()) {
-        // TODO: get product id from route
-        $body = 'PRODUCT 123456';
-        $title = 'Product 123456';
+        $controller = $productController;
     }
 }
+
+$params = $controller();
 
 ?>
 <!DOCTYPE html>
@@ -30,13 +53,13 @@ if ($homeMatch->isMatch()) {
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title><?php echo $title ?>$</title>
+    <title><?php echo $params['title'] ?>$</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 </head>
 <body>
     <div class="content">
         <main class="container">
-            <?php echo $body ?>
+            <?php echo $params['body'] ?>
         </main>
     </div>
 </body>
